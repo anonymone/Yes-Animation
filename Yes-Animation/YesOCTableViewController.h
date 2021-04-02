e9ddcc1516276fba8b47bcbd0c444aabd742d261
anonymone <severus.peng@gmail.com>
2021-04-02 22:12:28 +0800

    remove stroyboard

---
+++ Yes-Animation/YesOCTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  YesOCTableViewController.h
+//  Yes-Animation
+//
+//  Created by Severus Peng on 2021/4/2.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YesOCTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
